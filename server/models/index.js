6c073ff444847d654af8540e8d022fc5543731cc
admakinh <admakinh@gmail.com>
2022-05-25 19:59:51 -0400

    created models in server folder

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require("./User");
+const Kernel = require("./Kernel");
+const Collection = require("./Collection");
+const Habits = require("./Habits");
+
+module.exports = { User, Kernel, Habits, Collection };
