f91f27311cde5aa1b1838cecb57bc7cbed48c581
Angela Donati <angeladonati93@gmail.com>
2022-05-25 21:26:12 -0400

    resources cards

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -3,7 +3,39 @@ import React from "react";
 function Resources() {
   return (
     <>
-resources
+    <div className="mx-auto mt-4 mb-4"><h3>Resources</h3> </div>
+<div className="card-group">
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap"></img>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div className="card-footer">
+      <button className="btn btn-dark">Click</button>
+    </div>
+  </div>
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap"></img>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div className="card-footer">
+    <button className="btn btn-dark">Click</button>
+    </div>
+  </div>
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap"></img>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div className="card-footer">
+    <button className="btn btn-dark">Click</button>
+    </div>
+  </div>
+</div>
     </>
   );
 }
