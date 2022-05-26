1ba837e68a0ae3cfdd265f0b7f2e09aaff49880a
Marissa Flynn <mkflynn13@gmail.com>
2022-05-25 20:07:50 -0400

    paths in app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,34 @@ import "./App.css";
 // import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from './pages/login';
+import dailyTrackerPage from './pages/dailyTrackerPage';
+import gridPage from './pages/gridPage';
+import individualDay from './pages/individualDay';
+import resources from './pages/resources';
+import wordsOfEncouragement from './pages/wordsOfEncouragement';
+
 
 function App() {
   return (
 
-    <>      
-    <NavBar />
-      <div className="container">
-      </div>
-    </>
+    // <>      
+    // <NavBar />
+    //   <div className="container">
+    //   </div>
+    // </>
+    <Router>
+      <Navbar />
+    <Routes>
+        <Route exact path='/login' exact element={<Home />} />
+        <Route path='/dailytracker' element={<dailyTrackerPage/>} />
+        <Route path='/grid' element={<gridPage/>} />
+        <Route path='/individualday' element={<individualDay/>} />
+        <Route path='/resources' element={<resources/>} />
+        <Route path='/wordsofencouragement' element={<wordsOfEncouragement/>} />
+
+    </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/individualDay.js
@@ -0,0 +1,16 @@
+// Individual day (once user clicks on one grid square)
+// contains nav bar, title(header), a grid of clickable boxes (representing days)
+
+import React from "react";
+  
+const gridPage = () => {
+  return (
+    <div>
+      <h1>
+        Test words for the grid page.
+      </h1>
+    </div>
+  );
+};
+  
+export default gridPage;
