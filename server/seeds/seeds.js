8249e93de0ba58ea09830cc91da2c777b93ab471
admakinh <admakinh@gmail.com>
2022-05-25 21:21:18 -0400

    created seeds folder and seeds.js, built out framework for seed data todo make up fake users and data

---
+++ server/seeds/seeds.js
@@ -0,0 +1,27 @@
+const db = require("../config/connection");
+const { User, Habits, Kernel, Collection } = require("../models");
+const users = require("../seeds/users");
+const habits = require("../seeds/habits");
+const kernels = require("../seeds/kernels");
+const collections = require("../seeds/collections");
+
+db.once("open", async () => {
+  await User.deleteMany();
+  await User.insertMany([users]);
+  console.log("users seeded");
+
+  await Habits.deleteMany();
+  await Habits.insertMany([habits]);
+  console.log("habits seeded");
+
+  await Kernel.deleteMany();
+  await Kernel.insertMany([kernels]);
+  console.log("kernels seeded");
+
+  await Collection.deleteMany();
+  await Collection.insertMany([collections]);
+  console.log("collections seeded");
+
+  // ANDREW TO DO continue for all models
+  //   ANDREW TODO - add seed data for all models
+});
