7b7cd4211d0a5ea72f28eafae2f9da3c302108f6
Marissa Flynn <mkflynn13@gmail.com>
2022-06-04 14:28:51 -0400

    fixing client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import NavBar from './components/NavBar';
-import Login from './pages/login'
+// import Login from './pages/login'
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Home from './pages/login';
 import DailyTracker from './pages/dailyTrackerPage';
@@ -29,7 +29,7 @@ function App() {
       <NavBar />
       <div className="container">
     <Routes>
-        <Route exact path='/' element={<Login />} />
+        <Route exact path='/' element={<DailyTracker />} />
         <Route path='/dailytracker' element={<DailyTracker/>} />
         <Route path='/grid' element={<GridPage/>} />
         <Route path='/individualday' element={<IndividualDay/>} />
--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
-import 
+
 
 const initialList = [
   {
