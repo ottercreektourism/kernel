2e4a3b8df2d20e3fe659397610c98381eb681061
Angela Donati <angeladonati93@gmail.com>
2022-06-05 12:07:18 -0400

    update queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($firstName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, email: $email, password: $password) {
+      token
+      user {
+          _id
+          username
+          email
+          
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+          token
+          user {
+              _id
+          }
+      }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        
+    }
+}`;
