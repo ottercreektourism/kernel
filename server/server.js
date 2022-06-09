058172adb6ff15adcd1340e318d15dd4b6bebae1
Angela Donati <angeladonati93@gmail.com>
2022-06-08 20:39:07 -0400

    server.js

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`🌍 API server running on port ${PORT}!`);
